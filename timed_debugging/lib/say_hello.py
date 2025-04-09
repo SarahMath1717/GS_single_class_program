c1f1b20d662ab1485aa61c13d3e9957c63d73b4f
Sarah Mathieson <sarahmathieson@nationwide.co.uk>
2025-04-09 16:07:13 +0100

    test_1_done

--- timed_debugging/lib/say_hello.py
+++ timed_debugging/lib/say_hello.py
@@ -1,2 +1,2 @@
 def say_hello(name):
-    return "hello {name}"
+    return f"hello {name}"
--- timed_debugging/tests/test_say_hello.py
+++ timed_debugging/tests/test_say_hello.py
@@ -1,7 +1,5 @@
-from say_hello import *
-
-Intended output:
+from lib.say_hello import *
 
 def test_say_hello():
-    result = say_hello ("kay")
+    result = say_hello("kay")
     assert result == "hello kay"
